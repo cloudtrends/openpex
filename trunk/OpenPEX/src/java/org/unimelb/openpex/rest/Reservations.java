e85b010bb1c1e6822a310753098fbc3bdccf2387
jbroberg <jbroberg@71d0e584-2495-11de-8c12-6dbec8df7899>
2009-07-16 05:30:09 +0000

    Added scaffolding for RESTful Web Services.

--- trunk/OpenPEX/src/java/org/unimelb/openpex/rest/Reservations.java
+++ trunk/OpenPEX/src/java/org/unimelb/openpex/rest/Reservations.java
@@ -2,22 +2,28 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.unimelb.openpex.rest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.unimelb.openpex.VmUser;
+import org.unimelb.openpex.reservation.ReservationEntity;
+import org.unimelb.openpex.storage.PexStorage;
 
 /**
  *
  * @author brobergj
  */
 public class Reservations extends HttpServlet {
-   
+
+    PexStorage store = PexStorage.getInstance();
+
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
@@ -26,24 +32,45 @@ public class Reservations extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+            throws ServletException, IOException {
+
+        JSONObject jsonResponse = new JSONObject();
+
+        //response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json");
+
+
+
+        //String auth = request.getHeader("Authorization");
+        //String user = request.getHeader("OpenPEX-User");
+        //String pass = request.getHeader("OpenPEX-Pass");
+        String path = request.getPathInfo();
+        short user = Short.parseShort(path.substring(1));
+
         PrintWriter out = response.getWriter();
+
+        VmUser vmuser = store.getUserById(user);
+        List<ReservationEntity> reservations = store.getReservationsbyUserid(user);
+
+        
+
         try {
-            /* TODO output your page here
+            /* TODO output your page here */
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet Reservations</title>");  
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet Reservations at " + request.getContextPath () + "</h1>");
+            out.println(user);
             out.println("</body>");
             out.println("</html>");
-            */
-        } finally { 
+             
+        } finally {
             out.close();
         }
-    } 
+    }
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
@@ -55,9 +82,9 @@ public class Reservations extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
     /** 
      * Handles the HTTP <code>POST</code> method.
@@ -68,7 +95,7 @@ public class Reservations extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -80,5 +107,4 @@ public class Reservations extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
