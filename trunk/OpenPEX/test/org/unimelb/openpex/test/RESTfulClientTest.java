445123eb20cdb7a8a65f903e0d6ebd29b90576d6
jbroberg <jbroberg@71d0e584-2495-11de-8c12-6dbec8df7899>
2009-07-20 07:39:42 +0000

    Changed RESTful test client to a JUNIT test.

--- trunk/OpenPEX/test/org/unimelb/openpex/test/RESTfulClientTest.java
+++ trunk/OpenPEX/test/org/unimelb/openpex/test/RESTfulClientTest.java
@@ -4,8 +4,16 @@
  */
 package org.unimelb.openpex.test;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,20 +26,50 @@ import org.unimelb.openpex.reservation.ReservationEntity;
  *
  * @author brobergj
  */
-public class RESTfulClient {
+public class RESTfulClientTest {
 
-    String resEndpoint = "http://tyrellcorp.csse.unimelb.edu.au:8080/OpenPex/reservations/";
-    String instancesEndpoint = "http://tyrellcorp.csse.unimelb.edu.au:8080/OpenPex/reservations/";
+    static final String resEndpoint = "http://tyrellcorp.csse.unimelb.edu.au:8080/OpenPEX/reservations/";
+    static final String instancesEndpoint = "http://tyrellcorp.csse.unimelb.edu.au:8080/OpenPEX/instances/";
     String pexUser = "";
     String pexPass = "";
+
+    public RESTfulClientTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testListReservationsCall() throws MalformedURLException, IOException {
+        System.out.print(listReservationsCall());
+    }
 
     public String createReservationCall(String params) throws MalformedURLException, IOException {
         URL url = new URL(resEndpoint);
         InputStream in = url.openStream();
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+
         conn.addRequestProperty("OpenPEX-User", pexUser);
         conn.addRequestProperty("OpenPEX-Pass", pexPass);
-        conn.setRequestMethod("POST");
+
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setUseCaches(false);
+
         String type = conn.getContentType();
 
         return null;
@@ -51,15 +89,29 @@ public class RESTfulClient {
         return jsonResponse.toString(3);
     }
 
-    public String listReservationsCall(String params) throws MalformedURLException, IOException {
+    public String listReservationsCall() throws MalformedURLException, IOException {
         URL url = new URL(resEndpoint);
-        InputStream in = url.openStream();
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.addRequestProperty("OpenPEX-User", pexUser);
         conn.addRequestProperty("OpenPEX-Pass", pexPass);
         conn.setRequestMethod("GET");
-        String type = conn.getContentType();
 
-        return null;
+        conn.setDoInput(true);
+        conn.setUseCaches(false);
+        String response;
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()));
+        String line = reader.readLine();
+        StringBuffer content = new StringBuffer();
+        while (line != null) {
+            content.append(line + "\n");
+            line = reader.readLine();
+        }
+        response = content.toString();
+        reader.close();
+        conn.disconnect();
+
+        return response;
     }
 }
